14bd32ce6e1c381ac53255221720c09e837be4fc
Mario H. Adaniya <mhadaniya@gmail.com>
2020-06-25 20:11:35 -0300

    :pencil: aula 25-06 - app funcionando

---
+++ app_photo_classification/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:app_photo_classification/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
